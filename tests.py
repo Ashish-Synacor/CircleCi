7060085c1f401014b15ec5a409d2510925c26ecd
Ashish Verma <ashish.verma@synacor.com>
2023-11-22 16:47:00 +0530

    circleCI update name

--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ class MyTestCase(unittest.TestCase):
     def test_to_upper(self):
         name = "Ashish"
         upper_name = to_upper(name)
-        self.assertEqual(upper_name, "Ashish")
+        self.assertEqual(upper_name, "ASHISH")
 
 
 if __name__ == '__main__':
